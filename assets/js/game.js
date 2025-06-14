78f49717ccefc7fe346377a51ee6fa49dc65f184
Drake <dariocostarelli88@gmail.com>
2025-06-14 11:30:02 +0100

    GAME.JS: Refactor to use global variables for platforms, player, and cursors

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,7 @@
+var platforms;
+var player;
+var cursors;
+
 var config = {
   type: Phaser.AUTO,
   width: 800,
@@ -33,16 +37,16 @@ function create() {
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
 
-  //Star
+  // Star
   this.add.image(400, 300, 'star');
 
   // Player
   player = this.physics.add.sprite(100, 450, 'dude');
-  player.body.setGravityY(300);
-  this.physics.add.collider(player, platforms);
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
-  // Player movements
+  this.physics.add.collider(player, platforms);
+
+  // Animations
   this.anims.create({
     key: 'left',
     frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
@@ -62,6 +66,24 @@ function create() {
     frameRate: 10,
     repeat: -1,
   });
+
+  // Keyboard controls
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update() {}
+function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
+    player.anims.play('turn');
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+}

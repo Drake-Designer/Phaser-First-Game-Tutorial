4d0a67cabbc93aa79ad06aa85e650235b86f2cde
Drake <dariocostarelli88@gmail.com>
2025-06-14 11:17:54 +0100

    GAME.JS: Fixed platform position

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,7 +28,7 @@ function create() {
 
   // Static platforms
   platforms = this.physics.add.staticGroup();
-  platforms.create(400, 500, 'ground').setScale(2).refreshBody();
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
   platforms.create(600, 400, 'ground');
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');

c2cedd04b5531220ecafaf12869f81b081638291
Drake <dariocostarelli88@gmail.com>
2025-06-14 09:59:10 +0100

    GAME.JS: Add Phaser config and preload all game assets

---
+++ assets/js/game.js
@@ -0,0 +1,20 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: { preload, create, update },
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image('sky', 'assets/images/sky.png');
+  this.load.image('ground', 'assets/images/platform.png');
+  this.load.image('star', 'assets/images/star.png');
+  this.load.image('bomb', 'assets/images/bomb.png');
+  this.load.spritesheet('dude', 'assets/images/dude.png', { frameWidth: 32, frameHeight: 48 });
+}
+
+function create() {}
+
+function update() {}

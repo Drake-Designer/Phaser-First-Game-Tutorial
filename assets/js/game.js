177aa1655316d20c30701d05080a305739299cfa
Drake <dariocostarelli88@gmail.com>
2025-06-16 08:53:26 +0100

    HTML/CSS: Add #game-border div, responsive layout, and custom border for Phaser game

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,8 +12,13 @@ var gameOverBlinkEvent;
 
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  parent: 'game-container',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 600,
+  },
   physics: {
     default: 'arcade',
     arcade: {
